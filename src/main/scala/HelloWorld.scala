e448e5f3f33b1837507cd563b72df82f5a5eab93
Zoe <zoe.sanders@thomsonreuters.com>
2020-06-02 15:45:57 -0500

    Now print all lines of bee movie with word 'bee' in them

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,9 +1,16 @@
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object HelloWorld {
 
   def main(args: Array[String]): Unit ={
-    println(args(0))
-    var sc = SparkContext()
+    System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    val sparkConf = new SparkConf().setAppName("SOME APP NAME").setMaster("local[2]");
+
+    val sc = new SparkContext(sparkConf)
+    sc.setLogLevel("ERROR")
+    val data = sc.textFile("testFile.txt")
+    val words = data.flatMap(line => line.split("\n"))
+    val lineWithBee = words.filter(_.contains("bee"))
+    lineWithBee.foreach(println(_))
   }
 }
